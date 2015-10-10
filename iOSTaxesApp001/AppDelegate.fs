54743b92277e8a26f6274ca7d30204f915552acb
AnnMarieTorres <AnnMarieTorres@outlook.com>
2015-10-10 09:54:14 -0400

    Added Android and IOS
    Addes iOS and Android to F# PCL entire project is now F# with NO ERRORS

---
+++ iOSTaxesApp001/AppDelegate.fs
@@ -0,0 +1,23 @@
+﻿namespace iOSTaxesApp001
+
+open System
+open UIKit
+open Foundation
+
+[<Register("AppDelegate")>]
+type AppDelegate() = 
+    inherit UIApplicationDelegate()
+    member val Window = null with get, set
+    // This method is invoked when the application is ready to run.
+    override this.FinishedLaunching(app, options) = 
+        this.Window <- new UIWindow(UIScreen.MainScreen.Bounds)
+        // If you have defined a root view controller, set it here:
+        // this.Window.RootViewController <- new MyViewController ()
+        this.Window.MakeKeyAndVisible()
+        true
+
+module Main = 
+
+    let main args = 
+        UIApplication.Main(args, null, "AppDelegate")
+        0
